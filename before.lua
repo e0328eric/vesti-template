613441211f88979bb3ac0c607a7104fe440cd5d1
Sungbae Jeong <almagest0328@gmail.com>
2026-03-30 20:23:12 +0900

    change font

--- before.lua
+++ before.lua
@@ -1,20 +1,22 @@
+local function download_if_missing(url, dest)
+    local f <close> = io.open(dest, "r")
+    if not f then
+        vesti.download(url, dest)
+    end
+end
+
 local vesti_dummy = vesti.vestiDummyDir()
-
-local erewhon_url = "https://mirrors.ctan.org/fonts/erewhon/opentype/Erewhon-Regular.otf"
-local erewhon_bold_url = "https://mirrors.ctan.org/fonts/erewhon/opentype/Erewhon-Bold.otf"
-local erewhon_italic_url = "https://mirrors.ctan.org/fonts/erewhon/opentype/Erewhon-Italic.otf"
-
-local erewhon_math_url = "https://mirrors.ctan.org/fonts/erewhon-math/Erewhon-Math.otf"
-local erewhon_math_bold_url = "https://mirrors.ctan.org/fonts/erewhon-math/Erewhon-Math-Bold.otf"
-
 vesti.mkdir(vesti_dummy)
-vesti.download(erewhon_url, vesti.joinpath(vesti_dummy, "Erewhon-Regular.otf"))
-vesti.download(erewhon_bold_url, vesti.joinpath(vesti_dummy, "Erewhon-Bold.otf"))
-vesti.download(erewhon_italic_url, vesti.joinpath(vesti_dummy, "Erewhon-Italic.otf"))
 
-vesti.download(erewhon_math_url, vesti.joinpath(vesti_dummy, "Erewhon-Math.otf"))
-vesti.download(erewhon_math_bold_url, vesti.joinpath(vesti_dummy, "Erewhon-Math-Bold.otf"))
+local fonts_prefix = "https://mirrors.ctan.org/fonts/stix2-otf/"
 
--- if Vesti.engine_type() != "tect"
---     download_zip_font("Noto Fonts", "https://mirrors.ctan.org/fonts/noto.zip")
--- end
+local fonts = {
+    "STIXTwoText-Regular.otf",
+    "STIXTwoText-Bold.otf",
+    "STIXTwoText-Italic.otf",
+    "STIXTwoMath-Regular.otf",
+}
+
+for _, font in ipairs(fonts) do
+    download_if_missing(fonts_prefix .. font, vesti.joinpath(vesti_dummy, font))
+end
